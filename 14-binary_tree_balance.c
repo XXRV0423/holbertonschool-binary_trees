dd2a45b7904bbe04afd3825082bb244bbc92eedc
XXRV0423 <xxavierrv@gmail.com>
2026-04-09 01:17:25 -0400

    Bug fixed: typo error in the function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,5 +14,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	return ((int)binary_tree_height(tree->left) -
-			(int)binary_tree_heigh(tree->right));
+			(int)binary_tree_height(tree->right));
 }
