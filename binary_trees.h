9e6e484db114aae3df0f39c8fb78e97271bb2c87
XXRV0423 <xxavierrv@gmail.com>
2026-04-09 21:59:46 -0400

    Prototype for 16-binary_tree_is_perfect.c function added

--- binary_trees.h
+++ binary_trees.h
@@ -26,21 +26,57 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+/*Prototypes*/
+
+/*Task 0 - New node*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/*Task 1 - Insert left*/
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+/*Task 2 - Insert right*/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+/*Task 3 - Delete*/
 void binary_tree_delete(binary_tree_t *tree);
+
+/*Task 4 - Is leaf*/
 int binary_tree_is_leaf(const binary_tree_t *node);
+
+/*Task 5 - Is root*/
 int binary_tree_is_root(const binary_tree_t *node);
+
+/*Task 6 - Pre-order traversal*/
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+/*Task 7 - In-order traversal*/
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
+/*Task 8 - Post-order traversal*/
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+/*Task 9 - Height*/
 size_t binary_tree_height(const binary_tree_t *tree);
+
+/*Task 10 - Depth*/
 size_t binary_tree_depth(const binary_tree_t *tree);
+
+/*Task 11 - Size*/
 size_t binary_tree_size(const binary_tree_t *tree);
+
+/*Task 12 - Leaves*/
 size_t binary_tree_leaves(const binary_tree_t *tree);
+
+/*Task 13 - Nodes*/
 size_t binary_tree_nodes(const binary_tree_t *tree);
+
+/*Task 14 - Balance factor*/
 int binary_tree_balance(const binary_tree_t *tree);
+
+/*Task 15 - Is full*/
 int binary_tree_is_full(const binary_tree_t *tree);
+
+/*Task 16 - Is perfect*/
+int binary_tree_is_perfect(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
