a352cf75f72db5513bf6c2e05573fac4fcdd385b
XXRV0423 <xxavierrv@gmail.com>
2026-04-09 22:21:12 -0400

    Fix: add explicit single child check in binary_tree_is_perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -27,19 +27,17 @@ static size_t height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t left_h, right_h;
-
 	if (!tree)
-		return (0);
-
-	left_h = height(tree->left);
-	right_h = height(tree->right);
-
-	if (left_h != right_h)
 		return (0);
 
 	if (tree->left && !tree->right)
 		return (1);
+
+	if (!tree->left || !tree->right)
+		return (0);
+
+	if (height(tree->left) != height(tree->right))
+		return (0);
 
 	return (binary_tree_is_perfect(tree->left) &&
 			binary_tree_is_perfect(tree->right));
